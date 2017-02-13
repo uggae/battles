c83cdd60621014e2829a0a80ebbce5f988c14cd1
uggae <uggae@users.noreply.github.com>
2017-02-13 22:15:16 +0100

    initial code submit
    no idea

--- a_gift_well_spent.js
+++ a_gift_well_spent.js
@@ -1,1 +1,37 @@
-text
+var buy = function(x, arr) {
+  //generate pair values
+  console.log("Received +",x,arr);
+  var pairs=[];
+  //minimal difference found so far
+  var diff = {
+      val: x+1,
+      a: -1,
+      b: -1
+  };
+  console.log("-- diff=",diff.val,"(",diff.a,"),(",diff.b,")");
+
+  for (var i = 0 ; i < arr.length; i++ ) {
+      for (var j = 0 ; j < arr.length; j++ ) {
+           if (i>=j) {
+               pairs[i][j]=x+1;
+               console.log("[",i,"],[",j,"] = ",pairs[i][j]);
+              } else {
+                pairs[i][j] = arr[i] + arr[j];
+                console.log("[",i,"],[",j,"] = ",pairs[i][j]);
+                if (x-pairs[i][j] < diff.val) {
+                    diff.val = x-pairs[i][j];
+                    diff.a = i;
+                    diff.b = j;
+                    console.log("-- diff=",diff.val,"(",val.a,"),(",val.b,")");
+                };
+                //console.log("[",i,"],[",j,"] = ",pairs[i][j]," diff=",diff.val,"(",val.a,"),(",val.b,")");
+              }
+      }
+  };
+
+
+  // sort
+
+  //evaluate
+  return null;
+};
