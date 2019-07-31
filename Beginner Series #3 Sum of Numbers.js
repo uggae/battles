e3605e7109b537cf2a1d0865165f3c7156bfab85
uggae <uggae@uggae.net>
2019-07-31 02:17:22 +0200

    Codewars - Beginner Series #3 Sum of Numbers Sample tests passed

--- Beginner Series #3 Sum of Numbers.js
+++ Beginner Series #3 Sum of Numbers.js
@@ -1,4 +1,14 @@
 function GetSum( a,b )
 {
-  //Good luck!
+  var sum = 0
+  if (a<b) {
+    for (var i = a; i<=b; i++) {
+      sum += i
+    }
+  } else {
+    for (var i = b; i<=a; i++) {
+      sum += i
+    }
+  }
+  return sum
 }
