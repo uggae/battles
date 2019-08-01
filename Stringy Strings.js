7ff91624d054669375c2332cd69d7265b8c916e1
uggae <uggae@uggae.net>
2019-08-01 19:16:07 +0200

    Codewars - Stringy Strings
    minimized, kata passed

--- Stringy Strings.js
+++ Stringy Strings.js
@@ -1,14 +1,6 @@
 function stringy(size) {
- let gimme01s = function (length) {
-   console.log("Called with " + length)
    var out = "10"
-   for (var i = 0; i < length/2; i++) {
-     console.log("out is " + out)
+   for (var i = 0; i < size/2; i++)
      out = out.toString() + ("10")
-     }
-   console.log("Returning " + out.substr(0,length))
-   return out.substr(0,length)
- }
-
-return gimme01s(size);
+   return out.substr(0,size)
 }
