3e49467a7ac4d8ba8685f223c8ed1eb72e910b95
uggae <uggae@uggae.net>
2019-08-01 14:55:14 +0200

    Codewars - Beginner Series #3 Sum of Numbers Successfully passed kata

--- Beginner Series #3 Sum of Numbers.js
+++ Beginner Series #3 Sum of Numbers.js
@@ -1,14 +1,10 @@
 function GetSum( a,b )
 {
-// array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
   var sumSorted = function (start,end) {
     var sum = 0
-    //for (var i = start; i<=end; i++) sum += i
-    var i = start
-    while (i <= end) sum += i++
+    for (var i = start; i<=end; i++) sum += i
     return sum
   }
 
   return (a<b) ? sumSorted(a,b) : sumSorted(b,a)
-
 }
