56b3674ab3acdaca06c9355ef8c6505e6f12dc54
uggae <uggae@uggae.net>
2019-08-01 19:09:42 +0200

    Codewars - Stringy Strings
    passed initial tests
    dirty

--- Stringy Strings.js
+++ Stringy Strings.js
@@ -1,3 +1,14 @@
 function stringy(size) {
-  // your code here
+ let gimme01s = function (length) {
+   console.log("Called with " + length)
+   var out = "10"
+   for (var i = 0; i < length/2; i++) {
+     console.log("out is " + out)
+     out = out.toString() + ("10")
+     }
+   console.log("Returning " + out.substr(0,length))
+   return out.substr(0,length)
+ }
+
+return gimme01s(size);
 }
