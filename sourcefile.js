b9c5e9e37c382b3c0fb16de86f1d065ce105459b
uggae <uggae@uggae.net>
2019-08-06 18:38:00 +0200

    unittests  * .test() method in src .js file is supported (primary option)  * .test.js file is supported (secondary option)  * not yet implemented for all existing exercises

--- sourcefile.js
+++ sourcefile.js
@@ -1,4 +1,15 @@
 var exports = module.exports={}
 exports.mySum = function (a,b) {
   return a+b;
-};
+}
+exports.test = function () {
+  console.log(exports.mySum(1,2));
+  console.log(exports.mySum(0,0));
+  console.log(exports.mySum(10,200));
+}
+
+const assert = require('assert');
+
+assert.deepEqual([[[1, 2, 3]], 4, 5], [[[1, 2, '3']], 4, 5]);
+assert(true, "Should be true")
+// Generate an AssertionError to compare the error message later:
